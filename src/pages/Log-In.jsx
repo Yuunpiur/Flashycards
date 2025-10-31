4c744a77f0dfaf36ab6a4ddccc5d1ebbd64b7e7c
yuunp <pacanzachristopher@gmail.com>
2025-10-31 23:44:49 +0800

    finished designing the login and sign up page

--- src/pages/Log-In.jsx
+++ src/pages/Log-In.jsx
@@ -4,26 +4,29 @@ import lightningIcon from "#logo";
 function LogIn() {
   return (
     <>
-      <div className="flashycards-logo">
-        <img src={lightningIcon} className="lightning-icon" alt="shi" />
-        <div className="flashycards-text">FlashyCards</div>
-      </div>
-      <div className="log-in-container">
-        <div className="container-1">
-          <div className="log-in">
-            <input className="email-username" placeholder="Username or email" />
-            <input type="password" className="password" placeholder="Password" />
-          </div>
+      <div className="parent-container">
+        <div className="presentation-container">
+            
+            <div className="flashycards-text">FlashyCards</div>
+      
         </div>
-        <div className="container-2">
-          <div className="log-in-button-container">
-            <button className="log-in-button">Log In</button>
+        <div className="log-in-container">
+          <div className="container-1">
+            <div className="log-in">
+              <input className="email-username" placeholder="Username or email" />
+              <input type="password" className="password" placeholder="Password" />
+            </div>
           </div>
-          <div className="signup">
-            No Account?&nbsp;
-            <Link to="/Sign-Up" className="signup-link">
-              Create a new account
-            </Link>
+          <div className="container-2">
+            <div className="log-in-button-container">
+              <button className="log-in-button">Log In</button>
+            </div>
+            <div className="signup">
+              No Account?&nbsp;
+              <Link to="/Sign-Up" className="signup-link">
+                Create a new account
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Sign-up.jsx
+++ src/pages/Sign-up.jsx
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom";
-import lightningIcon from "#logo";
 import "../sign-up.css";
 
 function SignUp() {
   return (
     <>
-      <div className="flashycards-logo">
-        <img src={lightningIcon} className="lightning-icon" alt="shi" />
-        <div className="flashycards-text">FlashyCards</div>
-      </div>
-
-      <div className="sign-up-container">
-        <div className="sign-up">
-          <input className="email-username" placeholder="Username or email" />
-          <input type="password" className="password" placeholder="Password" />
-          <input type="password" className="confirm-password" placeholder="Confirm Password" />
+      <div className="parent-container">
+        <div className="flashycards-logo">
+          <div className="flashycards-text">FlashyCards</div>
         </div>
-        <div className="sign-up-button-and-login">
-          <button className="sign-up-button">Sign Up</button>
-          <div className="go-back-login">Go back to <Link to="/" className="gold-login">Log In</Link></div>
+
+        <div className="sign-up-container">
+          <div className="sign-up">
+            <input className="email-username" placeholder="Username or email" />
+            <input type="password" className="password" placeholder="Password" />
+            <input type="password" className="confirm-password" placeholder="Confirm Password" />
+          </div>
+          <div className="sign-up-button-and-login">
+            <button className="sign-up-button">Sign Up</button>
+            <div className="go-back-login">
+              Go back to{" "}
+              <Link to="/" className="gold-login">
+                Log In
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
