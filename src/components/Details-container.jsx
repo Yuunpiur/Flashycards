b2650632bd566a96e8c3f8fdb02a04ff76735705
yuunp <pacanzachristopher@gmail.com>
2025-11-24 14:46:10 +0800

    added some design ui for files and folders

--- src/components/Details-container.jsx
+++ src/components/Details-container.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import "../details-container.css";
 
-function DetailsContainer({blur, setBlur, folderName, setFolderName, category}) {
+function DetailsContainer({ blur, setBlur, folderName, setFolderName, category, setFolderIsEmpty }) {
   const [text, setText] = useState("");
-  
+
   const askFolder = () => {
-    // get the users folder name
-    // const folderName = prompt("Enter Name subject");
-    return text === "" || text === null ? "Unnamed Folder" : text;
-  }
+    // if the title is too long
+    if (text.length > 17) {
+      return text.slice(0, 14).concat("...");
+    } else {
+      return text === "" || text === null ? "Unnamed Folder" : text;
+    }
+  };
   const addFolder = () => {
+    setFolderIsEmpty(false);
     setFolderName([...folderName, askFolder()]);
     setText("");
   };
@@ -18,7 +22,12 @@ function DetailsContainer({blur, setBlur, folderName, setFolderName, category}) 
       {/* MOVE BOX UP WHEN BLUR */}
       <div className={`folder-info ${blur ? "move-up" : ""}`}>
         {/* TYPE THE NAME OF A FOLDER */}
-        <input className="folder-name" placeholder={category === "folders" ? "Folder name.." : "Topic name.."} value={text} onChange={(e) => setText(e.target.value)}></input>
+        <input
+          className="folder-name"
+          placeholder={category === "folders" ? "Folder name.." : "Topic name.."}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
         <div className="folder-nav-button">
           <div className="folder-exit-button" onClick={() => setBlur(!blur)}>
             EXIT
--- src/pages/Files-Folders.jsx
+++ src/pages/Files-Folders.jsx
@@ -2,6 +2,7 @@ import "../files-folders.css";
 import addFolderIcon from "#folder-icon";
 import DetailsContainer from "#components/Details-container.jsx";
 import Searchbar from "#components/Searchbar.jsx";
+import folderIcon from "../Pics/no-category/empty-folder.svg";
 
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -9,32 +10,48 @@ import { useState } from "react";
 function FilesFolder() {
   const [blur, setBlur] = useState(false);
   const [folderName, setFolderName] = useState([]);
-
-
- // move the folder info when clicked
-
+  const [folderIsEmpty, setFolderIsEmpty] = useState(folderName.length === 0);
+  console.log();
+  // move the folder info when clicked
   return (
     <>
       {/* BLUR THE BACKGROUND */}
       <div className={`files-folders-container ${blur ? "blur" : ""}`}>
         <div className="selection-bar">
-          <Searchbar icon={addFolderIcon} blur={blur} setBlur={setBlur}/>
-          
+          <Searchbar icon={addFolderIcon} blur={blur} setBlur={setBlur} />
+
           {/*   <button className="search-button">SEARCH</button> */}
         </div>
         <div className="rectangle-2 files-folders" id="rectangle-2">
           <div className="scroll-content-folders">
+            {folderIsEmpty ? (
+              <div className="message-folder-empty">
+                <img src={folderIcon} alt="" width={200} height={200} />
+                This page is empty, add some folders
+              </div>
+            ) : null}
             {/* FETCH AND DISPLAY ALL THE FOLDERS */}
             {folderName.map((folderName, index) => (
               <Link to="/Files-Folders/Topics" className="file-1 text-link" key={index}>
-                  <div className="folder-color"></div>
+                <div className="folder-color"></div>
+                <div className="folder-details-container">
                   <div className="file-1-title title">{folderName}</div>
+                  {/* THIS IS TEMPORARY */}
+                  <div className="total-topics-count">45 topics</div>
+                </div>
               </Link>
             ))}
           </div>
         </div>
       </div>
-      <DetailsContainer blur={blur} setBlur={setBlur} folderName={folderName} setFolderName={setFolderName} category={"folders"}/>
+      <DetailsContainer
+        blur={blur}
+        setBlur={setBlur}
+        folderName={folderName}
+        setFolderName={setFolderName}
+        category={"folders"}
+        setFolderIsEmpty={setFolderIsEmpty}
+      />
     </>
   );
 }
